ad2dfad08ec8399f5451a81a3974099b449bfeaa
minhpokemon212212-design <minhpokemon212212@gmail.com>
2026-07-15 16:47:39 +0700

    Create ShopAutomation.java

---
+++ src/main/java/me/addon/src/main/java/me/addon/ShopAutomation.java
@@ -0,0 +1,7 @@
+package me.addon;
+
+import meteordevelopment.meteorclient.systems.modules.Category;
+
+public class ShopAutomation {
+    public static final Category CATEGORY = new Category("Shop Automation");
+}
